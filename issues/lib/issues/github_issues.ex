8e283ad8e90eb9b096df5a296cd78a038865ce34
Maxim Chernyak <max@bitsonnet.com>
2015-10-11 11:28:35 -0400

    Build issues project up to table formatting part

---
+++ issues/lib/issues/github_issues.ex
@@ -0,0 +1,20 @@
+defmodule Issues.GithubIssues do
+  @user_agent [ {"User-agent", "Elixir app"} ]
+  @github_url Application.get_env(:issues, :github_url)
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({ :ok, %{ status_code: 200, body: body } }),
+    do: { :ok, :jsx.decode(body) }
+
+  def handle_response({ :ok, %{ status_code: ___, body: body } }),
+    do: { :error, :jsx.decode(body) }
+end
